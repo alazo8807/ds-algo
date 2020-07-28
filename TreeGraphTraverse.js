0060ada25bb332764ab602ff449c91ddb7258380
Alejo <alazo8807@gmail.com>
2020-07-28 14:16:55 -0400

    Add DFS method

--- TreeGraphTraverse.js
+++ TreeGraphTraverse.js
@@ -15,3 +15,22 @@ function BFS (node) {
   
   return result;
 }
+
+// Depth-First-Search (DFS) - RECURSIVELY
+function DFS (rootNode) {
+  const result = [];
+  
+  // PreOrder variant
+  function traverse (node) {
+      result.push(node); // (1)
+      if (node.left) traverse(node.left); // (2)
+      if (node.right) traverse(node.right); // (3)
+  }
+  
+  traverse(rootNode);
+  return result;
+}
+
+// Note: PostOrder Variant would be (2) (3) (1)
+// In-Order Variant would be (2) (1) (3)
+
